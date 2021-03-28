3444a5ba450f5e19fda4053102f829e20053b100
guilhermeassuncao <cgmatosassuncao@gmail.com>
2021-03-28 19:11:48 -0300

    bikcraft

---
+++ bikcraft/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Bikcraft</title>
+
+    <link rel="stylesheet" href="./public/css/normalize.css">
+    <link rel="stylesheet" href="./public/css/reset.css">
+    <link rel="stylesheet" href="./public/css/grid.css">
+    <link rel="stylesheet" href="./public/css/estilo.css">
+</head>
+<body>
+    <header>
+        <div>
+            <a href="/">
+                <img src="./public/img/bikcraft.svg" alt="Bikcraft">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/produtos">Produtos</a></li>
+                    <li><a href="/portfolio">Portfólio</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
