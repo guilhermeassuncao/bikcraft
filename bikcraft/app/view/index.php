2b2e76ee863bf943b772fed117a735a86fe8c336
guilhermeassuncao <cgmatosassuncao@gmail.com>
2021-03-29 07:50:24 -0300

    Introdução

---
+++ bikcraft/app/view/index.php
@@ -0,0 +1,12 @@
+<!-- Introdução -->
+<section class="introducao">
+    <div class="container">
+        <h1>BICICLETAS FEITAS A MÃO</h1>
+        <blockquote class="quote-externo">
+            <p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
+            <cite>WILLIAM MORRIS</cite>
+        </blockquote>
+        <a href="/produtos" class="btn">Orçamento</a>
+    </div>
+</section>
+<!-- Fim Introdução -->
--- bikcraft/index.php
+++ bikcraft/index.php
@@ -1,32 +1,48 @@
+<?php
+    require 'configs/header.php';
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <!-- Meta Tags -->
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!-- Fim Meta Tags -->
 
-    <title>Bikcraft</title>
+        <!-- CSS -->
+        <link rel="stylesheet" href="./public/css/normalize.css" />
+        <link rel="stylesheet" href="./public/css/reset.css" />
+        <link rel="stylesheet" href="./public/css/grid.css" />
+        <link rel="stylesheet" href="./public/css/estilo.css" />
+        <!-- Fim CSS -->
 
-    <link rel="stylesheet" href="./public/css/normalize.css">
-    <link rel="stylesheet" href="./public/css/reset.css">
-    <link rel="stylesheet" href="./public/css/grid.css">
-    <link rel="stylesheet" href="./public/css/estilo.css">
-</head>
-<body>
-    <header>
-        <div>
-            <a href="/">
-                <img src="./public/img/bikcraft.svg" alt="Bikcraft">
-            </a>
-            <nav>
-                <ul>
-                    <li><a href="/sobre">Sobre</a></li>
-                    <li><a href="/produtos">Produtos</a></li>
-                    <li><a href="/portfolio">Portfólio</a></li>
-                    <li><a href="/contato">Contato</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-</body>
+        <!-- Titulo -->
+        <title>Bikcraft</title>
+        <!-- Fim Titulo -->
+    </head>
+    <body>
+        <!-- Header -->
+        <header class="header">
+            <div class="container">
+                <a href="/" class="grid-4">
+                    <img src="./public/img/bikcraft.svg" alt="Bikcraft" />
+                </a>
+                <!-- Navegação -->
+                <nav class="header_menu grid-12">
+                    <ul>
+                        <li><a href="/sobre">Sobre</a></li>
+                        <li><a href="/produtos">Produtos</a></li>
+                        <li><a href="/portfolio">Portfólio</a></li>
+                        <li><a href="/contato">Contato</a></li>
+                    </ul>
+                </nav>
+                <!-- Fim Navegação -->
+            </div>
+        </header>
+        <!-- Fim Header -->
+        <?php  
+            require 'configs/section.php';
+        ?>
+    </body>
 </html>
